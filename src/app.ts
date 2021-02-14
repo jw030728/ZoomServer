6bdf7b994bf1c89f94bd7e2e071fa5f31cebd09e
jw030728 <jw1853063@gmail.com>
2021-02-14 12:56:44 +0900

    insomnia설치해서 학교에 대한 api 간단하게 만들어봄

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
-import {createServer, Server} from 'http';
+import { createServer, Server } from 'http';
+import bodyParser from 'body-parser';
+
+import controller from './controller';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(controller);    //모든 요청을 controller에게 보낼거임
 
 const server = createServer(app);
 server.listen(process.env.PORT || 5000);   
