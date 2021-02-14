e2ca6f5ff159cdf158a7da7749b802b368b51c83
jw030728 <jw1853063@gmail.com>
2021-02-14 09:23:20 +0900

    Add missing file

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+modules.export = {
+    extends: [
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
+    parserOption: {
+        project: './tsconfig.json',
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    globals: {
+        Atomics: 'readonl',
+        SharedArrayBuffer: 'readonly',
+    },
+    rules: {
+        'max-len': ['error', { code: 150, ignoreTemplateLiterals: true }],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/lines-between-class-members': 'off',
+        'import/prefer-default-exprot': 'off',
+    },
+};
